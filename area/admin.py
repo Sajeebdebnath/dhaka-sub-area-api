0859338037d590a691c3a937ac5d54e91316e318
Sajeeb Debnath <sajeebdebnath336@gmail.com>
2022-04-13 20:42:50 +0600

    Create Model & env file setup

--- area/admin.py
+++ area/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from area.models import DhakaSubArea
+
+
 # Register your models here.
+class DhakaSubAreaAdmin(admin.ModelAdmin):
+    list_display = ('name', 'bn_name', 'lat', 'lng')
+
+    
+admin.site.register(DhakaSubArea, DhakaSubAreaAdmin)
--- area/models.py
+++ area/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class DhakaSubArea(models.Model):
+    name = models.CharField(max_length=150)
+    bn_name = models.CharField(max_length=150)
+    lat = models.FloatField(max_length=128)
+    lng = models.FloatField(max_length=128)
+
+    def __str__(self):
+        return self.name
