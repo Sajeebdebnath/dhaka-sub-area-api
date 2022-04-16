b3be96e0753b15c374f33bf6bb137d1a51e68a5b
Sajeeb Debnath <sajeebdebnath336@gmail.com>
2022-04-16 21:46:10 +0600

    few changes

--- dhaka_sub_areas/settings.py
+++ dhaka_sub_areas/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [STATIC_ROOT_DIR]
+STATICFILES_DIRS = [STATIC_ROOT_DIR,]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
