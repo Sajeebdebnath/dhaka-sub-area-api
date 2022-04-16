eafe0d1ebb5e5bcc12ad7bcf2c8d188efb65093c
Sajeeb Debnath <sajeebdebnath336@gmail.com>
2022-04-16 23:20:36 +0600

    add white noise middleware

--- dhaka_sub_areas/settings.py
+++ dhaka_sub_areas/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'dhaka_sub_areas.urls'
